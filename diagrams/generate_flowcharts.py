b1126ae907b71278279562a0686c72aeea6bbf94
Sebastian Calderon <sebastiancalderoncarrillo@gmail.com>
2026-09-17 09:59:12 -0600

    Restructure and Add binaries

--- diagrams/generate_flowcharts.py
+++ diagrams/generate_flowcharts.py
@@ -4,4 +4,4 @@ todo = {"biseccion":["biseccion", "ejecutar_biseccion", "evaluar_polimonio"]}
 
 for file in todo:
     for func in todo[file]:
-        subprocess.run(["pyflowchart", f"src/modules/{file}.py", "-o", f"diagramas/{func}.html", "-f", func])
+        subprocess.run(["pyflowchart", f"../src/modules/{file}.py", "-o", f"{func}.html", "-f", func])
